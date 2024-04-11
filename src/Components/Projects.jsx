f8816d6fb8e4adf22c94ade2d451d971e2648ed6
Arpit Yadav <arpityadavay2005@gmail.com>
2024-04-11 17:43:04 +0530

    add hover effect in skill's grid

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Projects() {
   const card = (
-    <div className='h-48 w-80 rounded-sm bg-white/10'>
-        <h1 className=''>JavaScript Projects</h1>
+    <div className='h-48 w-80 rounded-sm bg-white/10 ease-in hover:border border-violet-400 duration-300 transition shadow-[0px_0px_20px_0px_rgba(0,0,0,0.3)] hover:shadow-violet-400'>
+        <h1 className='text-center mt-20'>JavaScript Projects</h1>
       </div>
   )
   return (
