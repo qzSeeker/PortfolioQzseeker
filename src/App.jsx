38e8bb9d2105bd717fd00629c2f57b8d4cbabd71
Arpit Yadav <arpityadavay2005@gmail.com>
2024-04-14 00:06:18 +0530

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import AboutMe from "./Components/AboutMe";
 import Blogs from "./Components/Blogs/Blogs";
+import Footer from "./Components/Footer";
 import Hero from "./Components/Hero";
 import Projects from "./Components/Projects/Projects";
 import Skills from "./Components/Skills";
@@ -14,6 +15,7 @@ function App() {
         <Skills />
         <Projects />
         <Blogs />
+        <Footer />
       </div>
     </>
   );
