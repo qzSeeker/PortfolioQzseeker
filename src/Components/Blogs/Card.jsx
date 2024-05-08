57cd57d674e54f171c38c8a06a82033beea186bf
Arpit Yadav <arpityadavay2005@gmail.com>
2024-05-08 23:05:18 +0530

    initial changes

--- src/Components/Blogs/Card.jsx
+++ src/Components/Blogs/Card.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 function Card({title, name, date, duration, link, img, ...otherProps}) {
     return (
-        <div className="h-[max] cursor-pointer w-full p-4 rounded-md bg-white/10 ease-in hover:border border-white/15 duration-300 transition shadow-md hover:shadow-violet-400 overflow-hidden">
+        <div className="h-[max] cursor-pointer w-full p-4 rounded-md bg-white/10 ease-in hover:border border-white/15 duration-300 transition-all shadow-md hover:shadow-violet-400 overflow-hidden">
             <h1 className="">{title}</h1>
             <p className='mt-2 text-sm'>
                 {date} • {duration}
--- src/Components/Projects/Cards.jsx
+++ src/Components/Projects/Cards.jsx
@@ -12,7 +12,7 @@ function Cards({
     ...otherProps
     }) {
     return (
-        <div className="h-max flex flex-col justify-around items-top p-6 w-full rounded-md bg-white/10 ease-in hover:border border-white/15 duration-300 transition shadow-md hover:shadow-violet-400 overflow-hidden">
+        <div className="h-max flex flex-col justify-around items-top p-6 w-full rounded-md bg-white/10 ease-in hover:border border-white/15 transition-all shadow-md hover:shadow-violet-400 overflow-hidden">
             <div className="flex justify-between items-center mb-4">
                 <div className="flex flex-col gap-4">
                     <h1 className="">{title}</h1>
