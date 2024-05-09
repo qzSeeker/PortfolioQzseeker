17a241fde5a531da2ec6366b5a7b5110a80afad1
Arpit Yadav <arpityadavay2005@gmail.com>
2024-05-09 15:11:15 +0530

    add framer motion

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { motion, useScroll, useSpring } from "framer-motion";
 import "./App.css";
 import AboutMe from "./Components/AboutMe";
 import Blogs from "./Components/Blogs/Blogs";
@@ -8,8 +9,16 @@ import Page from "./Components/Qualifications/Page";
 import Skills from "./Components/Skills";
 
 function App() {
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001
+  });
+
   return (
     <>
+      <motion.div className="bg-violet-400 fixed top-0 left-0 right-0 h-1" style={{ scaleX }}/>
       <div className="flex flex-col justify-center items-center">
         <Hero />
         <AboutMe />
